e21138e6240d2105f2bf900f6e4a0d1e9a605d3d
James Cui <jcui@coursehero.com>
2019-03-06 12:04:21 -0800

    init commit; ready for carthage

---
+++ TestProject/Objective-C-Regex-Categories-Carthage/Objective_C_Regex_Categories_Carthage.h
@@ -0,0 +1,18 @@
+//
+//  Objective_C_Regex_Categories_Carthage.h
+//  Objective-C-Regex-Categories-Carthage
+//
+//  Created by James Cui on 3/6/19.
+//  Copyright © 2019 Bendy Tree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Objective_C_Regex_Categories_Carthage.
+FOUNDATION_EXPORT double Objective_C_Regex_Categories_CarthageVersionNumber;
+
+//! Project version string for Objective_C_Regex_Categories_Carthage.
+FOUNDATION_EXPORT const unsigned char Objective_C_Regex_Categories_CarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Objective_C_Regex_Categories_Carthage/PublicHeader.h>
+#import <Objective_C_Regex_Categories_Carthage/RegExCategories.h>
